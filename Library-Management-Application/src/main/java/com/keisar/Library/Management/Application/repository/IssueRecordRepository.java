6f800f708c646ea5bd270c0682c7b94238f94fec
ade-teca <adelitoteca@gmail.com>
2026-02-05 03:17:07 -0300

    feat(MapperConfig): implement ModelMapper
    Implementação do Model Mapper para permitir a conversão automatizada no service

--- Library-Management-Application/src/main/java/com/keisar/Library/Management/Application/repository/IssueRecordRepository.java
+++ Library-Management-Application/src/main/java/com/keisar/Library/Management/Application/repository/IssueRecordRepository.java
@@ -2,6 +2,8 @@ package com.keisar.Library.Management.Application.repository;
 
 import com.keisar.Library.Management.Application.model.IssueRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IssueRecordRepository extends JpaRepository<IssueRecord, Long> {
 }
--- Library-Management-Application/src/main/java/com/keisar/Library/Management/Application/repository/UserRepository.java
+++ Library-Management-Application/src/main/java/com/keisar/Library/Management/Application/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.keisar.Library.Management.Application.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
