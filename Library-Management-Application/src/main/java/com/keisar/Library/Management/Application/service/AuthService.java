bbf1718dc4497b5109c03c8c69120681a04d1c49
ade-teca <adelitoteca@gmail.com>
2026-02-06 06:36:34 -0300

    feat: implement AuthService for user registration with encoded passwords
    Criados métodos para registro de usuários comuns (ROLE_USER) e administradores (ROLE_ADMIN).
    
    Implementada a criptografia de senhas utilizando PasswordEncoder.
    
    Configurada atribuição automática de permissões (Roles) durante o cadastro.
    
    Integrado ModelMapper para conversão de DTOs de registro em entidades.
    
    Criado método privado saveUser para centralizar a lógica de mapeamento e criptografia.
    
    Adicionada verificação de existência de e-mail antes do registo para evitar erros de base de dados.
    
    Melhorada a manutenção do código eliminando a redundância entre os métodos de registo.

---
+++ Library-Management-Application/src/main/java/com/keisar/Library/Management/Application/service/AuthService.java
@@ -0,0 +1,44 @@
+package com.keisar.Library.Management.Application.service;
+
+import com.keisar.Library.Management.Application.dto.request.LoginUserDTO;
+import com.keisar.Library.Management.Application.dto.request.RegisterUserDTO;
+import com.keisar.Library.Management.Application.dto.response.LoginResponse;
+import com.keisar.Library.Management.Application.model.User;
+import com.keisar.Library.Management.Application.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final ModelMapper modelMapper;
+
+    public void registerNormalUser(RegisterUserDTO registerUserDTO) {
+        saveUser(registerUserDTO, Set.of("ROLE_USER"));
+    }
+
+    public void registerAdminUser(RegisterUserDTO registerUserDTO) {
+        saveUser(registerUserDTO, Set.of("ROLE_USER", "ROLE_ADMIN"));
+    }
+
+    private void saveUser(RegisterUserDTO dto, Set<String> roles) {
+        if (userRepository.existsByEmail(dto.getEmail())) {
+            throw new RuntimeException("Email already in use");
+        }
+
+        User user = modelMapper.map(dto, User.class);
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        user.setRoles(roles);
+        userRepository.save(user);
+    }
+
+}
