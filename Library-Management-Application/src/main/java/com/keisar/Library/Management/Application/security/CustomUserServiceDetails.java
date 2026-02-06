7fe859d8b4a025d732d10cf855df9492c0a23ead
ade-teca <adelitoteca@gmail.com>
2026-02-06 09:16:27 -0300

    feat: implement CustomUserServiceDetails with email-based authentication
    Implementado loadUserByUsername para buscar usuários via e-mail.
    
    Adicionado tratamento de erro com UsernameNotFoundException para usuários inexistentes.
    
    Vinculado o repositório à engine de autenticação do Spring Security.

---
+++ Library-Management-Application/src/main/java/com/keisar/Library/Management/Application/security/CustomUserServiceDetails.java
@@ -0,0 +1,22 @@
+package com.keisar.Library.Management.Application.security;
+
+import com.keisar.Library.Management.Application.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserServiceDetails implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+    }
+}
