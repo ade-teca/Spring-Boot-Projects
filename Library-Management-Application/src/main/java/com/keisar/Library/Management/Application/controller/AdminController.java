0f679c00606bc6117c74c02f4be9144ab62d2c2b
ade-teca <adelitoteca@gmail.com>
2026-02-06 07:50:55 -0300

    feat: implement AdminController with null-safe updates and RBAC
    Adicionados endpoints de gestão administrativa: registro de admins, atualização e remoção de usuários.
    
    Implementada segurança via @PreAuthorize("hasRole('ADMIN')").
    
    Rotas padronizadas seguindo verbos REST e tratamento de status 204 (No Content) para deleções.
    
    Integrado com MapperConfig para garantir que atualizações parciais não corrompam dados existentes.

--- Library-Management-Application/src/main/java/com/keisar/Library/Management/Application/controller/AdminController.java
+++ Library-Management-Application/src/main/java/com/keisar/Library/Management/Application/controller/AdminController.java
@@ -1,4 +1,38 @@
 package com.keisar.Library.Management.Application.controller;
 
+
+import com.keisar.Library.Management.Application.dto.request.RegisterUserDTO;
+import com.keisar.Library.Management.Application.model.User;
+import com.keisar.Library.Management.Application.service.AdminService;
+import com.keisar.Library.Management.Application.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@PreAuthorize("hasRole('ADMIN')")
+@RequiredArgsConstructor
+@RequestMapping("/api/admin")
 public class AdminController {
+
+    private final AdminService adminService;
+    private final AuthService authService;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerAdmin(@RequestBody RegisterUserDTO userDTO) {
+        return ResponseEntity.status(201).body(authService.registerAdminUser(userDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody RegisterUserDTO userDTO) {
+        return ResponseEntity.ok(adminService.updateUser(id, userDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        adminService.deleteUser(id);
+        return ResponseEntity.status(204).build();
+    }
 }
