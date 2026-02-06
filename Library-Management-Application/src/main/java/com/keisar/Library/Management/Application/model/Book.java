22182e4684f06d8ce5b9413b34766928c5bb6b88
ade-teca <adelitoteca@gmail.com>
2026-02-06 05:17:11 -0300

    feat: implement automatic availability logic in Book entity
    Movida a regra de negócio de disponibilidade para o método setQuantity da entidade Book.
    
    Garante que isAvailable seja atualizado automaticamente sempre que a quantidade for alterada.
    
    Simplificada a lógica do BookService, removendo a necessidade de cálculos manuais de disponibilidade.

--- Library-Management-Application/src/main/java/com/keisar/Library/Management/Application/model/Book.java
+++ Library-Management-Application/src/main/java/com/keisar/Library/Management/Application/model/Book.java
@@ -9,7 +9,6 @@ import lombok.Setter;
 
 @Entity
 @Getter
-@Setter
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +18,25 @@ public class Book {
     private String isbn;
     private Long quantity;
     private boolean isAvailable;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+        this.isAvailable = (quantity != null && quantity > 0);
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
