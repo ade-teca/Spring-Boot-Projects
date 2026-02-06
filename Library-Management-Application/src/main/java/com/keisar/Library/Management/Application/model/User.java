e57034c39bb45157551a72326490333726321f61
ade-teca <adelitoteca@gmail.com>
2026-02-06 09:06:07 -0300

    feat: implement UserDetails in User entity for Spring Security integration
    Entidade User agora estende UserDetails.
    
    Implementada conversão de Roles para GrantedAuthority.
    
    Configurada persistência de permissões via @ElementCollection.
    
    Definidos métodos de status da conta para retorno padrão ativo.

--- Library-Management-Application/src/main/java/com/keisar/Library/Management/Application/model/User.java
+++ Library-Management-Application/src/main/java/com/keisar/Library/Management/Application/model/User.java
@@ -3,12 +3,18 @@ package com.keisar.Library.Management.Application.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Getter @Setter
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +26,28 @@ public class User {
     @ElementCollection(fetch = FetchType.EAGER)
     private Set<String> roles;
 
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email; // O Spring Security usará o e-mail como identificador único
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {return true;}
+
+    @Override
+    public boolean isAccountNonLocked() {return true;}
+
+    @Override
+    public boolean isCredentialsNonExpired() {return true;}
+
+    @Override
+    public boolean isEnabled() {return true;}
 }
